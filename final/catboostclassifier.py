5cf0aa001f8eee7e8214d38913d1489df62b9c5f
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:01:47 +1000

    comment

--- final/catboostclassifier.py
+++ final/catboostclassifier.py
@@ -2,6 +2,9 @@ from catboost import CatBoostClassifier
 from utils import predict
 
 def catboost(X_train, y_train):
+    '''
+    Final tuned model using CatBoost
+    '''
     cb_clf = CatBoostClassifier(random_seed=0, n_estimators=100, learning_rate=1.0, max_depth=10, verbose=False)
     cb_clf = cb_clf.fit(X_train, y_train)
 
