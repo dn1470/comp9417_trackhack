8a89e2b860e40daba0c3eef9e9046b7fc6cf1835
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:02:05 +1000

    Update decisiontree.py

--- final/decisiontree.py
+++ final/decisiontree.py
@@ -2,6 +2,9 @@ from sklearn import tree
 from utils import predict
 
 def decisiontree(X_train, y_train):
+    '''
+    Final tuned decision tree model
+    '''
     dt_clf = tree.DecisionTreeClassifier(random_state=0, min_samples_leaf=19)
     dt_clf = dt_clf.fit(X_train, y_train)
 
