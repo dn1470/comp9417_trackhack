f9287a759a4f95e77a9f336bdbc3dc6ec018a4c1
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:01:28 +1000

    comment

--- final/adaboost.py
+++ final/adaboost.py
@@ -2,6 +2,9 @@ from sklearn import ensemble
 from utils import predict
 
 def adaboost(X_train, y_train):
+    '''
+    Final tuned model using AdaBoost
+    '''
     adb_clf = ensemble.AdaBoostClassifier(random_state=0, n_estimators=125, learning_rate=1.25)
     adb_clf = adb_clf.fit(X_train, y_train)
 
