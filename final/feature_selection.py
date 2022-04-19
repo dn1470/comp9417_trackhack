f6b3162a31551bad774c3dfb40f772f9cf9ae79c
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:03:17 +1000

    Update feature_selection.py

--- final/feature_selection.py
+++ final/feature_selection.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def feature_selection(train_df_final, eval_set):
+    '''
+    Feature selector - drops the unimportant features
+    '''
     columns_to_drop=['storage_total', 'has_leased', 'storage_available', 'operating_system', 'bluetooth_on', 'memory_total']
     train_df_final = train_df_final.drop(columns=columns_to_drop)
     eval_set = eval_set.drop(columns=columns_to_drop)
