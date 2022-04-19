038d6c0c116b6750b69c008caf826060402a86f3
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:05:54 +1000

    Update xgboost.py

--- final/xgboost.py
+++ final/xgboost.py
@@ -2,6 +2,9 @@ import xgboost as xgb
 from utils import predict
 
 def xgboost(X_train, y_train):
+    '''
+    Final tuned model using XGBoostClassifier
+    '''
     xgb_clf = xgb.XGBClassifier(seed=0, alpha=0, learning_rate=0.1, colsample_bytree=0.25, max_depth=10, verbosity=0, use_label_encoder=False, eval_metric='logloss')
     xgb_clf = xgb_clf.fit(X_train, y_train)
 
