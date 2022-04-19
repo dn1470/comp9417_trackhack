9c71a4aa9609c96c8dc286735a58903070f68d4f
Kaman Chan <65635744+salty-armadillo@users.noreply.github.com>
2022-04-20 01:03:33 +1000

    Update histgradientboost.py

--- final/histgradientboost.py
+++ final/histgradientboost.py
@@ -2,6 +2,9 @@ from sklearn import ensemble
 from utils import predict
 
 def histgradientboost(X_train, y_train):
+    '''
+    Final tuned HistGradientBoostingClassifier
+    '''
     hgb_clf = ensemble.HistGradientBoostingClassifier(random_state=0, min_samples_leaf=5, learning_rate=0.5, max_depth=5, verbose=False)
     hgb_clf = hgb_clf.fit(X_train, y_train)
 
